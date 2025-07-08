a470154334f2de234db2fcc5ba00cde11c13fc3d
weberbau <d.weberbauer@gmx.net>
2025-07-08 20:15:23 +0200

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ import { useState } from "react";
 
 export default function App() {
   const [teams, setTeams] = useState([
-    { id: 1, name: "Schluckhalle", beers: 0 },
+    { id: 1, name: "Schluckhalle", beers: 10 },
     { id: 2, name: "FC Biercelona", beers: 0 },
     { id: 3, name: "TSV Bücherclub", beers: 0 },
-    { id: 4, name: "Police Academy", beers: 0 },
+    { id: 4, name: "Police Academy", beers: 3 },
     { id: 5, name: "FC Hopfenliebe", beers: 0 },
     { id: 6, name: "Droatbom", beers: 0 },
     { id: 7, name: "Chronisch Unterhopft", beers: 0 },
     { id: 8, name: "FC 80Ga Bertl", beers: 0 },
     { id: 9, name: "FC Koanz Hittn", beers: 0 },
-    { id: 10, name: "FV Vollsuff", beers: 0 },
+    { id: 10, name: "FV Vollsuff", beers: 5 },
     { id: 11, name: "Henahaufen", beers: 0 },
     { id: 12, name: "Hauptsoch s'Bier is Koid", beers: 0 },
     { id: 13, name: "Union Eschenau", beers: 0 },
